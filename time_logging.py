50a7bad0b21bf7f8d1237bb5a31949d038b016a0
maxxie3000 <max.goote@hotmail.com>
2020-06-03 18:52:34 +0200

    Added logging functionality

--- time_logging.py
+++ time_logging.py
@@ -3,17 +3,23 @@
 """
 Created on Tue Jun  2 19:16:41 2020
 
-fieldnames = ["Start_time", "End_time", "Activity", "Alternations", "Notes"]
+fieldnames = ["date","Start_time", "End_time", "Activity", "Alternations", "Notes"]
 
 @author: Max
 """
 import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import csv 
-from datetime import datetime
+from datetime import date, datetime
+import pathlib
+import os
 
 import user_interactions
 
-fieldnames = ["Start_time", "End_time", "Activity", "Alternations", "Notes"]
+fieldnames = ["Date", "Start_time", "End_time", "Activity", "Alternations", "Notes"]
+possible_activities = ['School', "Reading", "Phyton", "CFA", "German"]
+
 
 def get_last_line(url):
     with open(url, "r") as log:
@@ -35,8 +41,50 @@ def write_new_log(url, fieldnames):
         writer.writeheader()
         return "Log {0} is created".format(url)
 
-def write_start_time(start_time):
+def write_start(start):
     with open("logs/start_time.csv", "w") as starttime_log:
         writer = csv.writer(starttime_log, delimiter = ",")
-        writer.writerow([start_time])
+        writer.writerow(start)    
         
+def get_day_of_the_week():
+    weeknumber = date.today().isocalendar()[1]
+    return weeknumber 
+
+def check_if_log_exists(number):
+    path = pathlib.Path('log{0}.csv'.format(number))
+    return path.exists()
+
+def retrieve_start():
+    try: 
+        with open("logs/start_time.csv", "r") as start_time:
+            x = list(csv.reader(start_time, delimiter = ","))
+            os.remove("logs/start_time.csv")
+          #  x = np.array(x)
+            return x[0]
+    except FileNotFoundError:
+        return False
+
+def main_function():
+    start = retrieve_start()
+    
+    if not start == False:
+    
+        print(" Task ended ")
+        today = date.today()
+        time_ended = datetime.now().time()
+        time_started = start[0]
+        activity = start[1]
+        alternation, notes = user_interactions.ask_alternations_notes()
+        append_new_line("logs/log1.csv", [today, time_started, time_ended, activity, alternation, notes])
+        return
+    else:
+        n_activity = user_interactions.get_user_input(possible_activities)
+        activity = possible_activities[n_activity]
+        start_time = datetime.now().time()
+        write_start([start_time, activity])
+        print(" {0} started at {1} \n".format(activity, start_time))
+    return 
+
+main_function()
+            
+
--- user_interactions.py
+++ user_interactions.py
@@ -12,16 +12,55 @@ import time
 
 def get_user_input(possible_act):
     print("What activity are you going to do? ")
-    for n in possible_act:
-        print("{0}: for {1}".format(n, possible_act[n-1]))
-    activity = input("type the number of the activity: ")
-    try: 
-        n_activity = int(activity)
-    except: 
+    for n in range(len(possible_act)):
+        print("{0}: for {1}".format(n, possible_act[n]))  
+    activity = input("type the number of the activity: ")  
+    try:       
+       n_activity = int(activity)     
+    except:       
         print("\nThat's not a number!, Let's try it again\n")
-        activity = ""
-        time.sleep(1)
-        get_user_input(possible_act)
-    return possible_act[n_activity-1]
+        activity = ""  
+        time.sleep(1)      
+        n_activity = get_user_input(possible_act)  
+    return n_activity
     
+def ask_alternations_notes():
+    a = False
+    b = False   
+    
+    while a == False:
+        y_n_alt = input("\n Do you want to apply any alternations? Y/N\n").lower()
+        if( y_n_alt == "y"):      
+            alternation = get_alternation()
+            a = True
+        elif(y_n_alt == "n"):    
+            print("\n No alternations")
+            alternation = 0
+            a = True 
+        else:   
+            print("Wrong input")
+    
+    while b == False:
+        y_n_note = input("\n Do you want to add a note? Y/N").lower()
+        if( y_n_note == "y"):   
+            note = input("\n Provide your note:\n")
+            b = True
+        elif(y_n_note == "n"):  
+            print("\n No note")
+            note = ""
+            b = True    
+        else:       
+            print("wrong input")
+    return alternation, note
+        
+def get_alternation():
+    alternation = input("\n Provide your alternation in minutes:\n")        
+    try:         
+            n_alternation = int(alternation)        
+    except:             
+            print("\n That's not a number!, Let's try it again\n")            
+            alternation = ""    
+            time.sleep(1)        
+            n_alternation = get_alternation()        
+    return n_alternation
 
